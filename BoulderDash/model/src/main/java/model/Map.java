23f035da248999df1743186482f0bb75c5140ca6
Quentin <Quentin@Quentin>
2017-06-15 15:10:26 +0200

    TEst

---
+++ BoulderDash/model/src/main/java/model/Map.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Map {
+	private int width;
+	private int height;
+	
+	package Map(String fileName){
+		
+	}
+	
+	private void loadFile(String fileName){
+		return;
+	}
+	
+	public int getWidth(){
+		return (Integer) null;
+	}
+	
+	public int getHeight(){
+		return (Integer) null;
+	}
+	
+	public IElement getOnTheMapXY(int x, int y){
+		return null;
+	}
+	
+	public Observable getObservable(){
+		return null;
+	}
+	
+	private void setHeight(int height){
+		return;
+	}
+	
+	private void setWidth(int width){
+		return;
+	}
+	
+	private void setOnTheMapXY(IElement element, int x, int y){
+		return;
+	}
+	
+	public void setMobileHasChanged(){
+		return;
+	}
+	
+	
+	
+}
