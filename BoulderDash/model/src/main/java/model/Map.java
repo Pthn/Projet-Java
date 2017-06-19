9dbbcfb9fcb0fbf5ae1a52592f1640a5dc82eb27
Quentin <Quentin@Quentin>
2017-06-19 10:58:01 +0200

    map

--- BoulderDash/model/src/main/java/model/Map.java
+++ BoulderDash/model/src/main/java/model/Map.java
@@ -1,34 +1,58 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Observable;
 
-public class Map {
+import model.element.motionless.MotionlessElementsFactory;
+
+
+public class Map extends Observable implements IMap{
 	
 	private int width;
 	private int height;
 	
-	package map(String fileName){
-
+	Map(String fileName) throws IOException{
+		super();
+		this.loadFile(fileName);
 	}
 	
-	private void loadFile(String fileName){
-		return;
+	private void loadFile(String fileName) throws IOException{
+		final BufferedReader buffer = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+        String line;
+        int y = 0;
+        line = buffer.readLine();
+        this.setWidth(Integer.parseInt(line));
+        line = buffer.readLine();
+        this.setHeight(Integer.parseInt(line));
+        this.onTheMap = new IElement[this.getWidth()][this.getHeight()];
+        line = buffer.readLine();
+        while (line != null) {
+            for (int x = 0; x < line.toCharArray().length; x++) {
+                this.setOnTheMapXY(MotionlessElementsFactory.getFromFileSymbol(line.toCharArray()[x]), x, y);
+            }
+            line = buffer.readLine();
+            y++;
+        }
+        buffer.close();
 	}
 	
 	public int getWidth(){
-		return (Integer) null;
+		return this.width;
 	}
 	
 	public int getHeight(){
-		return (Integer) null;
+		return this.height;
 	}
 	
 	public IElement getOnTheMapXY(int x, int y){
-		return null;
+		return this.onTheMapXY[x][y];
 	}
 	
 	public Observable getObservable(){
-		return null;
+		return this;
 	}
 	
 	private void setHeight(int height){
@@ -40,11 +64,12 @@ public class Map {
 	}
 	
 	private void setOnTheMapXY(IElement element, int x, int y){
-		return;
+		this.onTheMap[x][y] = element;
 	}
 	
 	public void setMobileHasChanged(){
-		return;
+		this.setChanged();
+        this.notifyObservers();
 	}
 	
 	
