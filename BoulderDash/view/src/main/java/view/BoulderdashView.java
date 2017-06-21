1154c675096820b21093209e66854a48fa8bfe3d
Pthn <pierre.thenot@viacesi.fr>
2017-06-21 16:52:08 +0200

    view pierre

--- BoulderDash/view/src/main/java/view/BoulderdashView.java
+++ BoulderDash/view/src/main/java/view/BoulderdashView.java
@@ -1,40 +1,47 @@
 package view;
 
-import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
+import javax.swing.JPanel;
 
 import controller.IOrderPerformer;
 import controller.UserOrder;
 import model.IMap;
 import model.IMobile;
-import javax.swing.JFrame;
 
-public class BoulderdashView extends JFrame implements KeyListener, IBoulderdashView {
-	
+public class BoulderdashView<IElement> extends JFrame implements IBoulderdashView{
+
+	private static final long serialVersionUID = 1L;
 	private static int squareSize = 50;
 	public static int mapView = 10;
 	private Rectangle closeView;
 	private int view;
-	private IMap map;
+	public IMap map;
 	private IMobile myHero;
 	private IOrderPerformer orderPerformer;
 	private IMobile enemy;
+	private IElement[][] onTheMap;
 		
 		public BoulderdashView(final IMap map, final IMobile myHero){
 			 this.setTitle("Boulder Dash");
 		        
 		        this.setSize(getWidth(), getHeight());
 		        this.setLocationRelativeTo(null);
+		        JPanel pan = new JPanel();
+		        pan.
 		        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		        this.setVisible(true);
 		        this.setResizable(false);
-		        this.setContentPane(new Map());
+
+		        this.setContentPane(new pan);
 		        this.setVisible(true);
 		}
 //		{}
@@ -47,104 +54,31 @@ public class BoulderdashView extends JFrame implements KeyListener, IBoulderdash
 //		        this.setCloseView(new Rectangle(0, this.getMyHero().getY(), this.getMap().getWidth(), mapView));
 //		        SwingUtilities.invokeLater(this);
 //		}
+		 public IMap displayMap(Graphics g)
+		  {
 		
-		
-		
-		
+		    try
+		    {
+		      map mapdisplay = new map();
+		      //Pour une image de fond
+		      //g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+		    } catch (IOException e)
+		    {
+		      e.printStackTrace();
+		    }
+		  }
 		
 		public void displayMessage(final String message){
 	        JOptionPane.showMessageDialog(null, message);
 			
 		}
 		
-
-
-		import javax.swing.JFrame;
-
-
-		public void Window()
-		{
-		  
-	
-			
-		     
-		        this.setTitle("Boulder Dash");
-		        
-		        this.setSize(getWidth(), getHeight());
-		        this.setLocationRelativeTo(null);
-		        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		        this.setVisible(true);
-		        this.setResizable(false);
-		        this.setContentPane(new Map());
-		        this.setVisible(true);
-		     }
-		
-//		public void show(int yStart){
-//		     int y = yStart % this.Map();
-//		        for (int view = 0; view < this.getView(); view++) {
-//		            for (int x = 0; x < this.getMap().getWidth(); x++) {
-//		                if ((x == this.getMyHero().getX()) && (y == yStart)) {
-//		                    System.out.print(this.getMyHero().getSprite().getConsoleImage());
-//		                } else {
-//		                    System.out.print(this.getMap().getOnTheMapXY(x, y).getSprite().getConsoleImage());
-//		                }
-//		            }
-//		            y = (y + 1) % this.getMap().getHeight();
-//		            System.out.print("\n");
-//		        }
-//			
-//		}
-		public static UserOrder keyCodeToUserOrder(final int keyCode){
-			
-			UserOrder userOrder;
-	        switch (keyCode) {
-	            case KeyEvent.VK_RIGHT:
-	                userOrder = UserOrder.RIGHT;
-	                break;
-	            case KeyEvent.VK_LEFT:
-	                userOrder = UserOrder.LEFT;
-	                break;
-	            case KeyEvent.VK_UP:
-	                userOrder = UserOrder.UP;
-	                break;
-	            case KeyEvent.VK_DOWN:
-	                userOrder = UserOrder.DOWN;
-	                break;
-	            default:
-	                userOrder = UserOrder.NOP;
-	                break;
-	        }
-	        return userOrder;
-			
-		}
-		public void keyTyped(final KeyEvent keyEvent){
-			
-		}
-		public final void keyPressed(final KeyEvent keyEvent){
-			 try {
-		            this.getOrderPerformer().orderPerform(keyCodeToUserOrder(keyEvent.getKeyCode()));
-		        } catch (final IOException exception) {
-		            exception.printStackTrace();
-		        }
-		}
-		public void keyReleased(final KeyEvent keyEvent){
-			
-		}
 		public void followMyHero(){
 			this.getCloseView().y = this.getMyHero().getY() - 1;
 		}
 		
 		
-			public IMap getMap()
-			{
-				return this.map;
-			}
-			public void setMap(final IMap map)
-			{
-				this.map = map;
-		        
-		    
-			}
+			
 		
 			public IMobile getMyHero()
 			{
@@ -190,6 +124,62 @@ public class BoulderdashView extends JFrame implements KeyListener, IBoulderdash
 			{
 				this.enemy = enemy;
 			}
-		
 			
+//			public  void Window(IMap map)
+//			{			 
+//			        this.setTitle("Boulder Dash");
+//			        this.setSize(getWidth(), getHeight());
+//			        this.setLocationRelativeTo(null);
+//			        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//			        this.setVisible(true);
+//			        this.setResizable(false);
+//			        this.setContentPane((map) new map());
+//			        this.setVisible(true);
+//			     }
+			
+			public IMap getMap()
+			{
+				return this.map;
+			}
+			public void setMap(final IMap map)
+			{
+				this.map = map;
+		        
+		    
+			}
+			public static UserOrder keyCodeToUserOrder(final int keyCode){
+				
+				UserOrder userOrder;
+		        switch (keyCode) {
+		            case KeyEvent.VK_RIGHT:
+		                userOrder = UserOrder.RIGHT;
+		                break;
+		            case KeyEvent.VK_LEFT:
+		                userOrder = UserOrder.LEFT;
+		                break;
+		            case KeyEvent.VK_UP:
+		                userOrder = UserOrder.UP;
+		                break;
+		            case KeyEvent.VK_DOWN:
+		                userOrder = UserOrder.DOWN;
+		                break;
+		            default:
+		                userOrder = UserOrder.NOP;
+		                break;
+		        }
+		        return userOrder;}
+			public void keyTyped(final KeyEvent keyEvent){
+				
+			}
+			public final void keyPressed(final KeyEvent keyEvent){
+				 try {
+			            this.getOrderPerformer().orderPerform(keyCodeToUserOrder(keyEvent.getKeyCode()));
+			        } catch (final IOException exception) {
+			            exception.printStackTrace();
+			        }
+			}
+			public void keyReleased(final KeyEvent keyEvent){
+				
+			}
 }
+
