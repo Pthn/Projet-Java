8314e2197730aa341292b9abf1cc52ace05e7b05
Quentin <Quentin@Quentin>
2017-06-22 23:05:56 +0200

    View v2.0

--- BoulderDash/view/src/main/java/view/BoulderdashView.java
+++ BoulderDash/view/src/main/java/view/BoulderdashView.java
@@ -1,78 +1,138 @@
 package view;
 
 
-import java.awt.Graphics;
-import java.awt.LayoutManager;
+import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import controller.IOrderPerformer;
 import controller.UserOrder;
 import model.IMap;
 import model.IMobile;
-import model.IElement;
 
-public class BoulderdashView extends JFrame implements IBoulderdashView{
-
-	private static final long serialVersionUID = 1L;
-	private static int squareSize = 50;
-	public static int mapView = 10;
-	private Rectangle closeView;
-	private int view;
-	public IMap map;
-	private IOrderPerformer orderPerformer;
-	private IMobile myHero;
-	private IMobile enemy;
-	private IMobile blockDiamondFall;
-	private IMobile blockRocherFall;
-	private IMap diamond;
-	private int score = 0;
+//public class BoulderdashView extends JFrame implements IBoulderdashView{
+//
+//	private static final long serialVersionUID = 1L;
+//	private static int squareSize = 50;
+//	public static int mapView = 10;
+//	private Rectangle closeView;
+//	private int view;
+//	public IMap map;
+//	private IOrderPerformer orderPerformer;
+//	private IMobile myHero;
+//	private IMobile enemy;
+//	private IMobile blockDiamondFall;
+//	private IMobile blockRocherFall;
+//	private IMap diamond;
+//	private int score = 0;
+//	
+//		public BoulderdashView(final IMap map, final IMobile myHero){
+//				
+//				this.setView(mapView);
+//				this.setMap(map);
+//				this.setMyHero(myHero);
+//				((IElement) this.getMyHero()).getSprite();
+//				this.setEnemy(enemy);
+//				((IElement) this.getEnemy()).getSprite();
+//				this.setBlockDiamondFall(blockDiamondFall);
+//				((IElement) this.getBlockDiamondFall()).getSprite();
+//				this.setBlockRocherFall(blockRocherFall);
+//				((IElement) this.getBlockRocherFall()).getSprite();
+//				this.getDiamond();
+//				this.setDiamond(diamond);
+//				
+//				this.setCloseView(closeView);
+//			
+//				this.setTitle("Boulder Dash");
+//		        
+//		        this.setSize(getWidth(), getHeight());
+//		        this.setLocationRelativeTo(null);
+//		        JPanel pan = new JPanel((LayoutManager) map);
+//		        this.setContentPane(pan);
+//		        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		        this.setVisible(true);
+//		        this.setResizable(false);
+//
+//		        this.setContentPane(pan);
+//		        
+//		        this.setVisible(true);
+//		}
+//
+//		public void drawScore(Graphics g){
+//			g.drawString("Diamond = " + score, 25, 25);
+//			
+//			//add le if contact avec diament : score + 1 = score
+//		}
+		
 	
-		public BoulderdashView(final IMap map, final IMobile myHero){
-				
-				this.setView(mapView);
-				this.setMap(map);
-				this.setMyHero(myHero);
-				((IElement) this.getMyHero()).getSprite();
-				this.setEnemy(enemy);
-				((IElement) this.getEnemy()).getSprite();
-				this.setBlockDiamondFall(blockDiamondFall);
-				((IElement) this.getBlockDiamondFall()).getSprite();
-				this.setBlockRocherFall(blockRocherFall);
-				((IElement) this.getBlockRocherFall()).getSprite();
-				this.getDiamond();
-				this.setDiamond(diamond);
-				
-				this.setCloseView(closeView);
-			
-				this.setTitle("Boulder Dash");
-		        
-		        this.setSize(getWidth(), getHeight());
-		        this.setLocationRelativeTo(null);
-		        JPanel pan = new JPanel((LayoutManager) map);
-		        this.setContentPane(pan);
-		        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		        this.setVisible(true);
-		        this.setResizable(false);
+public class BoulderdashView implements  Runnable, KeyListener, IBoulderdashView {
+	private static final int mapView = 10;
+	private static final int squareSize = 50;
+	private Rectangle closeView;
+	private IMap map;
+	private IMobile Player;
+	private int view;
+	private IOrderPerformer orderPerformer;
+		
+	public BoulderdashView(final IMap Map, final IMobile Player) throws IOException{
+		this.setView(mapView);
+		this.setMap(Map);
+		this.setPlayer(Player);
+		this.getPlayer().getSprite().loadImage();
+		this.setCloseView(new Rectangle(0, this.getPlayer().getY(), this.getMap().getWidth(), mapView));
+		SwingUtilities.invokeLater(this);
+	}
 
-		        this.setContentPane(pan);
-		        
-		        this.setVisible(true);
-		}
+	@Override
+	public final void displayMessage(final String message) {
+        JOptionPane.showMessageDialog(null, message);
+	}
 
-		public void drawScore(Graphics g){
-			g.drawString("Diamond = " + score, 25, 25);
-			
-			//add le if contact avec diament : score + 1 = score
-		}
-		
-		
-		
+	@Override
+    public final void run() {
+    	final BoardFrame boardFrame = new BoardFrame("Boulder Dash fan Game Java A1");
+        boardFrame.setDimension(new Dimension(this.getMap().getWidth(), this.getMap().getHeight()));
+        boardFrame.setDisplayFrame(this.closeView);
+        boardFrame.setSize(this.closeView.width * squareSize, this.closeView.height * squareSize);
+        boardFrame.setHeightLooped(true);
+        boardFrame.addKeyListener(this);
+        boardFrame.setFocusable(true);
+        boardFrame.setFocusTraversalKeysEnabled(false);
+
+            for (int x = 0; x < this.getMap().getWidth(); x++) {
+                for (int y = 0; y < this.getMap().getHeight(); y++) {
+                    boardFrame.addSquare(this.map.getOnTheMapXY(x, y), x, y);
+                }
+            }
+            boardFrame.addPawn(this.getPlayer());
+
+            this.getMap().getObservable().addObserver(boardFrame.getObserver());
+            //this.followPlayer();
+
+            boardFrame.setVisible(true);
+        }
+	
+	
+    public final void show(final int yStart){
+    	int y = yStart % this.getMap().getHeight();
+    	for (int view = 0; view < this.getView(); view++){
+    		for(int x = 0; x < this.getMap().getWidth(); x++){
+    			if((x == this.getPlayer().getX()) && (y == yStart)){
+    				System.out.print(this.getPlayer().getSprite().getConsoleImage());
+    			}else{
+    				System.out.print(this.getMap().getOnTheMapXY(x, y).getSprite().getConsoleImage());
+    			}
+    		}
+    		y = (y+ 1) % this.getMap().getHeight();
+    		System.out.print("\n");
+    	}
+    }
+
 //		        SwingUtilities.invokeLater(this);
 //		}
 //		 public IMap displayMap(Graphics g)
@@ -87,88 +147,88 @@ public class BoulderdashView extends JFrame implements IBoulderdashView{
 //		    {
 //		      e.printStackTrace();
 //		    }
-//		  }
-		
-		public void displayMessage(final String message){
-	        JOptionPane.showMessageDialog(null, message);
-			
-		}
-		
-		public void followMyHero(){
-//			this.getCloseView().y = this.getMyHero().getY() - 1;
-	}
-		
-		
-			
-		
-			public IMobile getMyHero()
-			{
-				return this.getMyHero();
-			}
-			public void setMyHero(final IMobile myHero)
-			{
-				this.myHero = myHero;
-			}
-			
-			public int getView()
-			{
-				return this.view;
-			}
-			public void setView(final int view)
-			{
-				this.view = view;
-			}
-
-			public Rectangle getCloseView()
-			{
-				return this.closeView;
-			}
-			public void setCloseView(final Rectangle closeView)
-			{
-				this.closeView = closeView;
-			}
-			
-			public IOrderPerformer getOrderPerformer()
-			{
-				return this.orderPerformer;
-			}
-			public void setOrderPerformer(final IOrderPerformer orderPerformer)
-			{
-				this.orderPerformer = orderPerformer;
-			}
-			
-			public IMobile getEnemy()
-			{
-				return this.enemy;
-			}
-			public void setEnemy(final IMobile enemy)
-			{
-				this.enemy = enemy;
-			}
-			
-			public IMobile getBlockDiamondFall(){
-				return this.blockDiamondFall;
-			}
-			
-			public void setBlockDiamondFall(final IMobile blockDiamondFall){
-				this.blockDiamondFall = blockDiamondFall;
-			}
-			
-			public IMobile getBlockRocherFall(){
-				return this.blockRocherFall;
-			}
-			
-			public void setBlockRocherFall(final IMobile blockRocherFall){
-				this.blockRocherFall = blockRocherFall;
-			}
-			
-			public IMap getDiamond(){
-				return diamond;
-			}
-			
-			public void setDiamond(IMap diamond){
-				this.diamond = diamond;
-			}
+////		  }
+//		
+//		public void displayMessage(final String message){
+//	        JOptionPane.showMessageDialog(null, message);
+//			
+//		}
+//		
+//		public void followMyHero(){
+////			this.getCloseView().y = this.getMyHero().getY() - 1;
+//	}
+//		
+//		
+//			
+//		
+//			public IMobile getMyHero()
+//			{
+//				return this.getMyHero();
+//			}
+//			public void setMyHero(final IMobile myHero)
+//			{
+//				this.myHero = myHero;
+//			}
+//			
+//			public int getView()
+//			{
+//				return this.view;
+//			}
+//			public void setView(final int view)
+//			{
+//				this.view = view;
+//			}
+//
+//			public Rectangle getCloseView()
+//			{
+//				return this.closeView;
+//			}
+//			public void setCloseView(final Rectangle closeView)
+//			{
+//				this.closeView = closeView;
+//			}
+//			
+//			public IOrderPerformer getOrderPerformer()
+//			{
+//				return this.orderPerformer;
+//			}
+//			public void setOrderPerformer(final IOrderPerformer orderPerformer)
+//			{
+//				this.orderPerformer = orderPerformer;
+//			}
+//			
+//			public IMobile getEnemy()
+//			{
+//				return this.enemy;
+//			}
+//			public void setEnemy(final IMobile enemy)
+//			{
+//				this.enemy = enemy;
+//			}
+//			
+//			public IMobile getBlockDiamondFall(){
+//				return this.blockDiamondFall;
+//			}
+//			
+//			public void setBlockDiamondFall(final IMobile blockDiamondFall){
+//				this.blockDiamondFall = blockDiamondFall;
+//			}
+//			
+//			public IMobile getBlockRocherFall(){
+//				return this.blockRocherFall;
+//			}
+//			
+//			public void setBlockRocherFall(final IMobile blockRocherFall){
+//				this.blockRocherFall = blockRocherFall;
+//			}
+//			
+//			public IMap getDiamond(){
+//				return diamond;
+//			}
+//			
+//			public void setDiamond(IMap diamond){
+//				this.diamond = diamond;
+//			}
 			
 			
 //			public  void Window(IMap map)
@@ -183,49 +243,114 @@ public class BoulderdashView extends JFrame implements IBoulderdashView{
 //			        this.setVisible(true);
 //			     }
 			
-			public IMap getMap()
-			{
-				return this.map;
+//			public IMap getMap()
+//			{
+//				return this.map;
+//			}
+//			public void setMap(final IMap map)
+//			{
+//				this.map = map;
+//		        
+//		    
+//			}
+    public static UserOrder keyCodeToUserOrder(final int keyCode){
+				
+    	UserOrder userOrder;
+			switch (keyCode) {
+			case KeyEvent.VK_RIGHT:
+				userOrder = UserOrder.RIGHT;
+				break;
+			case KeyEvent.VK_LEFT:
+				userOrder = UserOrder.LEFT;
+		        break;
+			case KeyEvent.VK_UP:
+				userOrder = UserOrder.UP;
+		        break;
+		    case KeyEvent.VK_DOWN:
+		        userOrder = UserOrder.DOWN;
+		        break;
+		    default:
+		    	userOrder = UserOrder.NOP;
+		        break;
 			}
-			public void setMap(final IMap map)
-			{
-				this.map = map;
-		        
+		    return userOrder;
 		    
-			}
-			public static UserOrder keyCodeToUserOrder(final int keyCode){
+    }
+			
+	@Override		
+	public void keyTyped(final KeyEvent keyEvent){
 				
-				UserOrder userOrder;
-		        switch (keyCode) {
-		            case KeyEvent.VK_RIGHT:
-		                userOrder = UserOrder.RIGHT;
-		                break;
-		            case KeyEvent.VK_LEFT:
-		                userOrder = UserOrder.LEFT;
-		                break;
-		            case KeyEvent.VK_UP:
-		                userOrder = UserOrder.UP;
-		                break;
-		            case KeyEvent.VK_DOWN:
-		                userOrder = UserOrder.DOWN;
-		                break;
-		            default:
-		                userOrder = UserOrder.NOP;
-		                break;
-		        }
-		        return userOrder;}
-			public void keyTyped(final KeyEvent keyEvent){
-				
+	}
+	
+	public final void keyPressed(final KeyEvent keyEvent){
+		try {
+			this.getOrderPerformer().orderPerform(keyCodeToUserOrder(keyEvent.getKeyCode()));
+        } catch (final IOException exception) {
+        	exception.printStackTrace();
+        }
 			}
-			public final void keyPressed(final KeyEvent keyEvent){
-				 try {
-			            this.getOrderPerformer().orderPerform(keyCodeToUserOrder(keyEvent.getKeyCode()));
-			        } catch (final IOException exception) {
-			            exception.printStackTrace();
-			        }
+		
+//	public void keyReleased(final KeyEvent keyEvent){
+//				
+//			}
+			
+	private IMap getMap(){
+		return this.map;
+   	}
+
+   	private void setMap(final IMap map) throws IOException {
+   		this.map = map;
+	    for (int x = 0; x < this.getMap().getWidth(); x++) {
+	    	for (int y = 0; y < this.getMap().getHeight(); y++) {
+	    		this.getMap().getOnTheMapXY(x, y).getSprite().loadImage();
+	    	}
+	    }
+   	}
+
+   	private IMobile getPlayer(){
+   		return this.Player;
+   	}
+
+   	private void setPlayer(final IMobile Player){
+   		this.Player = Player;
+   	}
+
+   	private int getView(){
+   		return this.view;
+   	}
+
+   	private void setView(final int view){
+   		this.view = view;
+   	}
+
+	       /*	private Rectangle getCloseView(){
+	       		return this.closeView;
+	       	}*/
+
+   	private void setCloseView(final Rectangle closeView){
+   		this.closeView = closeView;
+   	}
+
+   	private IOrderPerformer getOrderPerformer(){
+   		return this.orderPerformer;
+   	}
+
+	public final void setOrderPerformer(final IOrderPerformer orderPerformer) {
+	       		this.orderPerformer = orderPerformer;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
 			}
-			public void keyReleased(final KeyEvent keyEvent){
-				
-			}
-}
+
+	}
+
 
